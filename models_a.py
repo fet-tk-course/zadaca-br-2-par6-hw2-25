b40d0dec6b81299fbcbe93f9675584195d0237c5
Šejla <sejla.valjevac@fet.ba>
2026-05-08 16:38:23 +0200

    Kreiran Product model i pripadajuce sheme

--- models_a.py
+++ models_a.py
@@ -1,5 +1,25 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
-# TODO: Student A - Definiši svoj SQLModel entitet ovdje
-# 
+class Product(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    category: str
+    quantity: int
+    price: float
+    available: bool
+    
+class ProductCreate(SQLModel):
+    name: str
+    category: str
+    quantity: int
+    price: float
+    available: bool
+    
+class ProductUpdate(SQLModel):
+    name: Optional[str] = None
+    category: Optional[str] = None
+    quantity: Optional[int] = None
+    price: Optional[float] = None
+    available: Optional[bool] = None
+
