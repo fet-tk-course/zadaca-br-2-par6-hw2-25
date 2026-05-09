7696b7c1ac35fd74c757b192910f7942dcf26813
Edna <edna.avdic@fet.ba>
2026-05-09 13:02:31 +0200

    POST - Kreiranje dobavljača

--- routes_b.py
+++ routes_b.py
@@ -1,6 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
-
+from typing import List, Optional
 from database import get_session
+from models_b import Supplier, SupplierCreate, SupplierUpdate
 
-router = APIRouter(prefix="/resursi_b", tags=["Resurs B"])
+router = APIRouter(prefix="/suppliers", tags=["Suppliers"])
+
+
+@router.post("/", response_model=Supplier, status_code=status.HTTP_201_CREATED)
+def create_supplier(supplier: SupplierCreate, session: Session = Depends(get_session)):
+    # Kreiranje instance modela iz primljenih podataka
+    db_supplier = Supplier.model_validate(supplier)
+    session.add(db_supplier)
+    session.commit()
+    session.refresh(db_supplier)
+    return db_supplier
