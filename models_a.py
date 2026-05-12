0b750a5036855fd0003a1bb4efe640752f69f175
Šejla <sejla.valjevac@fet.ba>
2026-05-12 20:09:22 +0200

    Dodani komentari

--- models_a.py
+++ models_a.py
@@ -1,6 +1,7 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
+#Model proizvoda koji predstavlja tabelu u bazi podataka
 class Product(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
@@ -9,7 +10,8 @@ class Product(SQLModel, table=True):
     price: float
     available: bool
     supplier_id: int
-    
+
+#Model za kreiranje proizvoda   
 class ProductCreate(SQLModel):
     name: str
     category: str
@@ -18,6 +20,7 @@ class ProductCreate(SQLModel):
     available: bool
     supplier_id: int
     
+#Model za ažuriranje proizvoda
 class ProductUpdate(SQLModel):
     name: Optional[str] = None
     category: Optional[str] = None
